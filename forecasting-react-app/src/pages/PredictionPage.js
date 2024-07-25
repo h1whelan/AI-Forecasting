ae4f023dc9501ee72b3100db2ed90a45b75ebf5f
HWhelan <hwhelan@capeannam.com>
2024-07-25 09:06:50 +0100

    Memoize the getAiSuggestedQuestions function using useCallback

--- forecasting-react-app/src/pages/PredictionPage.js
+++ forecasting-react-app/src/pages/PredictionPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getAiResponseStream } from '../services/aiService';
 import { submitResponse } from '../services/apiService';
